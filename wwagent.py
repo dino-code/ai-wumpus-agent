fdab568a526876c1be3088e91bad9201791d8611
Dino <dbecaj@fordham.edu>
2020-11-28 14:12:23 -0500

    added updateFrontier method, self.known, and self.frontier

--- wwagent.py
+++ wwagent.py
@@ -43,6 +43,8 @@ class WWAgent:
         self.directions=['up','right','down','left']
         self.facing = 'right'
         self.arrow = 1
+        self.frontier = [((3, 1), 0, 0), ((2, 0), 0, 0)] # ((row, col), hasPit, hasWumpus). The 2 default frontier elements will always be the case
+        self.known = [((3,0), 0, 0)] # all the squares appended to this list will have values of 0 (otherwise the game would end).
         self.percepts = (None, None, None, None, None)
         self.map = [[ self.percepts for i in range(self.max) ] for j in range(self.max)]
         print("New agent created")
@@ -75,6 +77,10 @@ class WWAgent:
             self.position = (self.position[0], min(self.max-1, self.position[1]+1))
         elif self.facing =='left':
             self.position = (self.position[0], max(0,self.position[1]-1))
+        
+        if (self.position, 0, 0) not in self.known:
+                self.known.append((self.position, 0, 0)) # append the new position to known positions
+
         return self.position
 
     # and the same is true for the direction the agent is facing, it also
@@ -103,6 +109,43 @@ class WWAgent:
             else:
                 self.facing = 'up'
 
+    # this function finds the squares adjacent to the current square
+    # and appends them to self.frontier
+
+    def updateFrontier(self):
+        temp = []
+
+        # first, we must remove values from the frontier if
+        # we have visited them already (because then they)
+        # are no longer in the frontier
+        duplicates = [i for i in self.frontier if i in self.known]
+
+        for duplicate in duplicates:
+            self.frontier.pop(self.frontier.index(duplicate)) # remove visited squares from frontier
+
+        above = (self.position[0]+1, self.position[1])
+        below = (self.position[0]-1, self.position[1])
+        left = (self.position[0], self.position[1]-1)
+        right = (self.position[0], self.position[1]+1)
+        
+        # check to see if the dimension are between 
+        # 0 and self.max, otherwise, they aren't
+        # valid dimensions
+        if above[0] < self.max:
+            temp.append(above)
+        if below[0] > -1:
+            temp.append(below)
+        if left[1] > -1:
+            temp.append(left)
+        if right[1] < self.max:
+            temp.append(right)
+
+        # go through all adjacent squares and check to see if
+        # the adjacent squares are not already in the frontier
+        # and haven't already been visited
+        for square in temp:
+            if (square, 0, 0) not in self.frontier and (square, 0, 0) not in self.known:
+                self.frontier.append((square, 0, 0))
 
     # this is the function that will pick the next action of
     # the agent. This is the main function that needs to be
@@ -121,6 +164,11 @@ class WWAgent:
             print("Agent will grab the gold!")
             self.stopTheAgent=True
             return 'grab'
+        
+        # the frontier is updated here and then we check
+        # what the best move should be based on the new
+        # frontier
+        self.updateFrontier()
         
         # choose a random direction, and move          
         actionSelection = randint(0,1)
