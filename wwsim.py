b671dff408f828c6dc7d8c87226c073257a25b5c
Dino <dbecaj@fordham.edu>
2020-12-04 18:52:33 -0500

    adding wwsim.py

--- wwsim.py
+++ wwsim.py
@@ -6,7 +6,6 @@
 # Includes classes for the simulation and the Tkinter display of the simulation.
 # Also includes code to process the command-line input and run the program accordingly.
 
-
 import sys
 
 from wwagent import *
@@ -123,18 +122,6 @@ class Simulation:
                 self.percepts['room'+str(r)+str(c)] = (None, None, None, None, None)
 
     def agent_move(self, action):
-        """
-
-        ***DELETE BEFORE SUBMISSION***
-
-        This is here for testing purposes --> reveals the agent's percepts as it navigates the environment
-
-        print("-"*20)
-        for i in self.agent.map:
-            print(i)
-        print("-"*20)
-
-        """
         if (action == 'exit'):
             self.endEpisode=True
             return
